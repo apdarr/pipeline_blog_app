c3bc6c1b8280c70639600109a1fbfbfbb1189c8f
Alex Darr <adarr@adarr-ltm.internal.salesforce.com>
2016-03-29 10:57:22 -0400

    configure routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get 'new' => 'posts#new'
+
+  get 'edit' => 'posts#edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
